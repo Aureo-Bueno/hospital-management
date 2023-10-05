9b17764bd471d998c64bab057c166f07fff2892d
Aureo-Bueno <aureoalexandre@outlook.com>
2023-10-05 20:09:48 -0300

    feat: add boilerplate

---
+++ Hospital.Web/Program.cs
@@ -0,0 +1,26 @@
+WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddControllersWithViews();
+}
+
+WebApplication? app = builder.Build();
+{
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
+}
